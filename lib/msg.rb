dde2a4718232f60767de03650ac83b6b42cb6906
Katrina Owen <kytrinyx@github.com>
2017-04-17 13:36:57 -0400

    Handle errors for missing CSV data
    Only throw the error if we actually need it in the template.

--- lib/msg.rb
+++ lib/msg.rb
@@ -1,9 +1,17 @@
 class Msg
+  class MissingAttribute < StandardError; end
+
   attr_reader :csv_row, :subject, :body
   def initialize(csv_row, contents)
     @csv_row = csv_row
     csv_row.each do |key, value|
-      contents = contents.gsub("{{%s}}" % key, value)
+      placeholder = "{{%s}}" % key
+      next if !contents.include?(placeholder)
+
+      if value.nil?
+        raise MissingAttribute, "missing value for '%s' attribute" % key
+      end
+      contents = contents.gsub(placeholder, value)
     end
 
     @subject, *lines = contents.split("\n")
--- test/msg_test.rb
+++ test/msg_test.rb
@@ -14,5 +14,27 @@ class MsgTest < Minitest::Test
     assert_equal "Fun fact about California", msg.subject
     assert_equal "The state bird in California is quail. We serve quail fried.", msg.body
   end
+
+  def test_nil_csv_data_is_fine_when_not_used_in_template
+    headers = %w(name number favorite)
+    data = %w(Alice one)
+
+    template = "{{name}} is number {{number}}."
+    row = CSV::Row.new(headers, data)
+    msg = Msg.new(row, template)
+    assert_equal "Alice is number one.", msg.subject
+  end
+
+  def test_nil_csv_data_blows_up_if_template_needs_it
+    headers = %w(name number favorite)
+    data = %w(Alice one)
+
+    template = "{{number}}: {{name}} likes {{favorite}}."
+    row = CSV::Row.new(headers, data)
+    e = assert_raises(Msg::MissingAttribute) do
+      msg = Msg.new(row, template)
+    end
+    assert_equal "missing value for 'favorite' attribute", e.message
+  end
 end
 
